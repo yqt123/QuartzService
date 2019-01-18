2892f5a3ed84f71d2579e235ec49fa2179713fd7
yqt123 <864083509@qq.com>
2019-01-18 16:13:02 +0800

    1

--- QuartzService/Program.cs
+++ QuartzService/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Quartz.Core.IOC;
+using Quartz.Core.Owin;
+using Quartz.Core.Quartz;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceProcess;
@@ -14,12 +17,34 @@ namespace QuartzService
         /// </summary>
         static void Main()
         {
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[]
-            {
-                new QuartzService()
-            };
-            ServiceBase.Run(ServicesToRun);
+            //ServiceBase[] ServicesToRun;
+            //ServicesToRun = new ServiceBase[]
+            //{
+            //    new QuartzService()
+            //};
+            //ServiceBase.Run(ServicesToRun);
+            Start();
+        }
+
+        static void Start()
+        {
+            ObjectContainer.ApplicationStart(new Quartz.Core.IOC.AutoFacContainer());
+
+            //ITest _ITest = ObjectContainer.Current.Resolve<ITest>();
+            //_ITest.SayHello();
+
+            //Console.ReadLine();
+
+            var pcScheduler = Scheduler.Create();
+            pcScheduler.Start();
+
+
+            OwinHelper owinHelper = OwinHelper.Create("8099");
+            owinHelper.Start();
+            Console.ReadLine();
+
+            //Quartz.Core.Log4net.Log4.Info("test.......");
+            //Console.ReadLine();
         }
     }
 }
---
+++ QuartzWebNancy/Modules/ApiModule.cs
@@ -0,0 +1,63 @@
+﻿using Nancy;
+using Nancy.Helpers;
+using QuartzWebNancy.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuartzWebNancy.Modules
+{
+    public class ApiModule : BaseModule
+    {
+        public ApiModule()
+        {
+            Post["/nancy/api"] = parameters => ReturnStringAction();
+        }
+
+        public dynamic ReturnStringAction()
+        {
+            var json = GetParameters();
+            return json;
+        }
+
+        protected string GetParameters()
+        {
+            if (Request.Body.Length == 0)
+                return "";
+            if (Request.Headers.ContentType.Contains("multipart/form-data"))
+            {
+                string args = string.Empty;
+                if (Request.Form["formJson"] != null)
+                {
+                    args = Request.Form["formJson"].ToString();
+                }
+                else
+                {
+                    args = "{";
+                    for (int n = 0; n < Request.Form.Count; n++)
+                    {
+                        string name = Request.Form.Keys[n].ToString();
+                        string value = "'" + Request.Form[n].ToString() + "'";
+                        if (name == "ResquestModel" && !string.IsNullOrEmpty(Request.Form[n].ToString()))
+                        {
+                            value = Request.Form[n].ToString();
+                        }
+                        if (n == Request.Form.Count - 1)
+                            args += string.Format("'{0}':{1}", name, value);
+                        else
+                            args += string.Format("'{0}':{1},", name, value);
+                    }
+                    args += "}";
+                }
+                return args;
+            }
+
+            byte[] byts = new byte[Request.Body.Length];
+            Request.Body.Read(byts, 0, byts.Length);
+            return System.Text.Encoding.UTF8.GetString(byts);
+        }
+    }
+}
